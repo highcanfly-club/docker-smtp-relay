07dee981c9011bb658560aa4a2f29765dbc5c313
Ronan <ronan.le_meillat@sctg.eu.org>
2023-02-18 15:59:01 +0000

    add letsencrypt auto certificate

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM boky/postfix
 LABEL maintainer="Ronan Le Meillat <ronan@parapente.cf>"
-RUN apk --no-cache add curl &&\
-    echo "*/2     *       *       *       *       /update-cloudflare-dns.sh" >> /etc/crontabs/root
+RUN apk --no-cache add curl acme.sh &&\
+    echo "*/2     *       *       *       *       /update-cloudflare-dns.sh" >> /etc/crontabs/root &&\
+    echo "0     0       *       *       0       acme.sh --renew-all --config-home /listmonk/backups/certs/config" >> /etc/crontabs/root 
 ADD scripts/dkim-parsing.sh /docker-init.db/
 ADD scripts/init-cloudflare.sh /docker-init.db/
+ADD scripts/init-letsencrypt.sh /docker-init.db/
 ADD scripts/update-cloudflare-dns.sh /
---
+++ scripts/init-letsencrypt.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+. /scripts/common.sh
+. /scripts/common-run.sh
+
+CERT_DIR=/listmonk/backups/certs
+if [ ! -f $CERT_DIR/$POSTFIX_HOSTNAME.key ]; then
+    mkdir -p $CERT_DIR/config
+    export CF_Token="$CLOUDFLARE_API_KEY"
+    acme.sh --issue -d $POSTFIX_HOSTNAME --dns dns_cf --ocsp-must-staple --config-home $CERT_DIR/config \
+                                --keylength 4096 --server letsencrypt \
+                                --cert-file $CERT_DIR/$POSTFIX_HOSTNAME.pem --key-file $CERT_DIR/$POSTFIX_HOSTNAME.key \
+                                --fullchain-file $CERT_DIR/$POSTFIX_HOSTNAME-full.pem
+else
+    echo "CERT: $POSTFIX_HOSTNAME.pem exists…"
+    acme.sh --renew-all --config-home $CERT_DIR/config
+    postconf -e "smtpd_tls_cert_file=$CERT_DIR/$POSTFIX_HOSTNAME-full.pem"
+    postconf -e "smtpd_tls_key_file=$CERT_DIR/$POSTFIX_HOSTNAME.key"
+    postconf -e "smtp_tls_cert_file=$CERT_DIR/$POSTFIX_HOSTNAME-full.pem"
+    postconf -e "smtp_tls_key_file=$CERT_DIR/$POSTFIX_HOSTNAME.key"
+    postconf -P submission/inet/smtpd_tls_wrappermode=yes
+fi
