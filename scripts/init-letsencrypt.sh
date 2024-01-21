1423037329029721162bc1a4bc1206a3c3774185
Your Name <you@example.com>
2024-01-21 19:17:43 +0100

    change staging location

--- scripts/init-letsencrypt.sh
+++ scripts/init-letsencrypt.sh
@@ -4,15 +4,15 @@
 if [[ $USE_LETSENCRYPT -eq '1' ]]; then
     if [[ $USE_LETSENCRYPT_STAGING -eq '1' ]]; then
         echo "Using Let's Encrypt staging server"
-        export STAGING="--staging"
+        STAGING="--staging"
     fi
     CERT_DIR=/etc/ssl
     if [ ! -f $CERT_DIR/$POSTFIX_HOSTNAME.key ]; then
         sleep $(($(od -vAn -N2 -tu2 </dev/urandom) % 60))
         mkdir -p $CERT_DIR/acme.config
         export CF_Token="$CLOUDFLARE_API_KEY"
-        acme.sh --issue -d $POSTFIX_HOSTNAME $STAGING --dns dns_cf --ocsp-must-staple --config-home $CERT_DIR/config \
-            --keylength 4096 --server letsencrypt \
+        acme.sh --issue -d $POSTFIX_HOSTNAME --dns dns_cf --ocsp-must-staple --config-home $CERT_DIR/config \
+            --keylength 4096 --server letsencrypt $STAGING \
             --cert-file $CERT_DIR/certs/$POSTFIX_HOSTNAME.pem --key-file $CERT_DIR/private/$POSTFIX_HOSTNAME.key \
             --fullchain-file $CERT_DIR/certs/$POSTFIX_HOSTNAME-full.pem
     fi
